0f15dd7bf8e7ffdef28ac6922a926e71192e5e48
LeonardoPirchio <116616781+LeonardoPirchio@users.noreply.github.com>
2023-02-21 02:18:26 -0300

    Update

--- layout/main.js
+++ layout/main.js
@@ -56,7 +56,7 @@ function playPause() {
 /* ========================
 * ======== Chamada ========
 =========================== */
-const TIME_TO_SHOW_BUTTON = 582; // 588 seconds;
+const TIME_TO_SHOW_BUTTON = 426; // 588 seconds;
 const video = document.getElementById('myVideo');
 
 video.addEventListener('timeupdate', function showButton() {
@@ -99,28 +99,28 @@ window.addEventListener("orientationchange", function() {
 
 // Inicie o rastreamento quando o vídeo for carregado
 video.addEventListener("loadedmetadata", function() {
-   fbq('trackCustom', 'Loaded');
+   // fbq('trackCustom', 'Loaded');
    gtag('event', 'loaded', {'event_category': 'Video','event_label': 'Loaded','value':'Video-01'});
 //   console.log('Loaded')
 });
 
 // Rastreie a reprodução do vídeo
 video.addEventListener("play", function() {
-   fbq('trackCustom', 'Play');
+   // fbq('trackCustom', 'Play');
    gtag('event', 'play', {'event_category': 'Video','event_label': 'Play','value':'Video-01'});
 //   console.log('Play')
 });
 
 // Rastreie a pausa do vídeo
 video.addEventListener("pause", function() {
-   fbq('trackCustom', 'Pause');
+   // fbq('trackCustom', 'Pause');
    gtag('event', 'pause', {'event_category': 'Video','event_label': 'Pause','value':'Video-01'});
 //   console.log('Pause')
 });
 
 // Rastreie o final do vídeo
 video.addEventListener("ended", function() {
-   fbq('trackCustom', 'Ended');
+   // fbq('trackCustom', 'Ended');
    gtag('event', 'ended', {'event_category': 'Video','event_label': 'Ended','value':'Video-01'});
 //   console.log('Ended')
 });
@@ -143,7 +143,7 @@ video.addEventListener("timeupdate", retencao_Oferta);
 function video_10seg(){
    var timePassed = video.currentTime;
    if (timePassed >= 10 && timePassed < 60) {
-      fbq('trackCustom', 'Tempo_10seg');
+      // fbq('trackCustom', 'Tempo_10seg');
       gtag('event', 'Tempo_10seg', {'event_category': 'Video','event_label': 'Tempo_10seg','value':'Video-01'});
       video.removeEventListener("timeupdate", video_10seg);
    }
@@ -151,7 +151,7 @@ function video_10seg(){
 function video_01min(){
    var timePassed = video.currentTime;
    if (timePassed >= 60 && timePassed < 120) {
-      fbq('trackCustom', 'Tempo_01min');
+      // fbq('trackCustom', 'Tempo_01min');
       gtag('event', 'Tempo_01min', {'event_category': 'Video','event_label': 'Tempo_01min','value':'Video-01'});
       video.removeEventListener("timeupdate", video_01min);
    }
@@ -159,7 +159,7 @@ function video_01min(){
 function video_02min(){
    var timePassed = video.currentTime;
    if (timePassed >= 120 && timePassed < 180) {
-      fbq('trackCustom', 'Tempo_02min');
+      // fbq('trackCustom', 'Tempo_02min');
       gtag('event', 'Tempo_02min', {'event_category': 'Video','event_label': 'Tempo_02min','value':'Video-01'});
       video.removeEventListener("timeupdate", video_02min);
    }
@@ -167,7 +167,7 @@ function video_02min(){
 function video_03min(){
    var timePassed = video.currentTime;
    if (timePassed >= 180 && timePassed < 240) {
-      fbq('trackCustom', 'Tempo_03min');
+      // fbq('trackCustom', 'Tempo_03min');
       gtag('event', 'Tempo_03min', {'event_category': 'Video','event_label': 'Tempo_03min','value':'Video-01'});
       video.removeEventListener("timeupdate", video_03min);
    }
@@ -175,7 +175,7 @@ function video_03min(){
 function video_04min(){
    var timePassed = video.currentTime;
    if (timePassed >= 240 && timePassed < 300) {
-      fbq('trackCustom', 'Tempo_04min');
+      // fbq('trackCustom', 'Tempo_04min');
       gtag('event', 'Tempo_04min', {'event_category': 'Video','event_label': 'Tempo_04min','value':'Video-01'});
       video.removeEventListener("timeupdate", video_04min);
    }
@@ -183,7 +183,7 @@ function video_04min(){
 function video_05min(){
    var timePassed = video.currentTime;
    if (timePassed >= 300 && timePassed < 360) {
-      fbq('trackCustom', 'Tempo_05min');
+      // fbq('trackCustom', 'Tempo_05min');
       gtag('event', 'Tempo_05min', {'event_category': 'Video','event_label': 'Tempo_05min','value':'Video-01'});
       video.removeEventListener("timeupdate", video_05min);
    }
@@ -191,7 +191,7 @@ function video_05min(){
 function video_06min(){
    var timePassed = video.currentTime;
    if (timePassed >= 360 && timePassed < 420) {
-      fbq('trackCustom', 'Tempo_06min');
+      // fbq('trackCustom', 'Tempo_06min');
       gtag('event', 'Tempo_06min', {'event_category': 'Video','event_label': 'Tempo_06min','value':'Video-01'});
       video.removeEventListener("timeupdate", video_06min);
    }
@@ -199,7 +199,7 @@ function video_06min(){
 function video_07min(){
    var timePassed = video.currentTime;
    if (timePassed >= 420 && timePassed < 480) {
-      fbq('trackCustom', 'Tempo_07min');
+      // fbq('trackCustom', 'Tempo_07min');
       gtag('event', 'Tempo_07min', {'event_category': 'Video','event_label': 'Tempo_07min','value':'Video-01'});
       video.removeEventListener("timeupdate", video_07min);
    }
@@ -207,7 +207,7 @@ function video_07min(){
 function video_08min(){
    var timePassed = video.currentTime;
    if (timePassed >= 480 && timePassed < 540) {
-      fbq('trackCustom', 'Tempo_08min');
+      // fbq('trackCustom', 'Tempo_08min');
       gtag('event', 'Tempo_08min', {'event_category': 'Video','event_label': 'Tempo_08min','value':'Video-01'});
       video.removeEventListener("timeupdate", video_08min);
    }
@@ -215,7 +215,7 @@ function video_08min(){
 function retencao_Oferta(){
    var tempo = video.currentTime;
    if (tempo >= 425) {
-      fbq('trackCustom', 'retencao_Oferta');
+      // fbq('trackCustom', 'retencao_Oferta');
       gtag('event', 'retencao_Oferta', {'event_category': 'Video','event_label': 'retencao_Oferta','value':'Video-01'});
       video.removeEventListener("timeupdate", retencao_Oferta);
    }
